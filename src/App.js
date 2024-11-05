320d1f3af4fae8dc322803a4585df1690ff50a01
aljog786 <aljog786@gmail.com>
2024-11-05 22:34:00 +0530

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,45 @@
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutUs from './pages/AboutUs';
 
 function App() {
 
-  const [ feedback,setFeedback ] = useState(FeedbackData);
+  const [feedback, setFeedback] = useState(FeedbackData);
 
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
-    setFeedback([newFeedback,...feedback]);
+    setFeedback([newFeedback, ...feedback]);
   }
 
   const deleteFeedback = (id) => {
-    if (window.confirm('r u sure ?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+    if (window.confirm('Are you sure?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback}/>
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          } />
+          
+          <Route path='/aboutus' element={<AboutUs />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+import Card from "../components/shared/Card"
+
+const AboutUs = () => {
+  return (
+    <Card>
+        <div className="about">
+            <h1>About this project</h1>
+            <p>This is a feedback app</p>
+            <p>version : 1.0</p>
+            <p><a href="/">back to home</a></p>
+        </div>
+    </Card>
+  )
+}
+export default AboutUs
